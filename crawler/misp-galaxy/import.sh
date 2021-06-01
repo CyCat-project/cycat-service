0a3695c5d5ddac2ef93ffcc99695d1ae5332f4cb
Alexandre Dulaunoy <a@foo.be>
2021-06-01 11:10:50 +0200

    chg: [crawler] misp galaxy more default import

--- crawler/misp-galaxy/import.sh
+++ crawler/misp-galaxy/import.sh
@@ -1,3 +1,5 @@
 python3 misp_galaxy_importer.py -u https://raw.githubusercontent.com/MISP/misp-galaxy/main/clusters/threat-actor.json
 python3 misp_galaxy_importer.py -u https://raw.githubusercontent.com/MISP/misp-galaxy/main/clusters/tool.json
 python3 misp_galaxy_importer.py -u https://raw.githubusercontent.com/MISP/misp-galaxy/main/clusters/malpedia.json
+python3 misp_galaxy_importer.py -u https://raw.githubusercontent.com/MISP/misp-galaxy/main/clusters/ransomware.json
+python3 misp_galaxy_importer.py -u https://raw.githubusercontent.com/MISP/misp-galaxy/main/clusters/backdoor.json
